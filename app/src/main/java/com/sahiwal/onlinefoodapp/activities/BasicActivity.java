f922d891aec3c445517224df037436e0e67a12d7
Fiaz Zafar <fiazzafar430@gmail.com>
2025-02-22 08:02:48 -0800

    initial commit1

---
+++ app/src/main/java/com/sahiwal/onlinefoodapp/activities/BasicActivity.java
@@ -0,0 +1,15 @@
+package com.sahiwal.onlinefoodapp.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BasicActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
---
+++ app/src/main/java/com/sahiwal/onlinefoodapp/activities/IntroActivity.java
@@ -0,0 +1,22 @@
+package com.sahiwal.onlinefoodapp.activities;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Window;
+import com.sahiwal.onlinefoodapp.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends BasicActivity {
+
+    ActivityIntroBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getWindow().setStatusBarColor(Color.parseColor("#FFE4B5"));
+
+        binding.signUpBtn.setOnClickListener(v ->{});
+        binding.loginBtn.setOnClickListener(v ->{});
+    }
+}
